7338737f0e5059073c1a0eea5b1771f9c4f8bb96
Praneeth <vhpraneeth2001@gmail.com>
2024-04-25 17:11:57 +0530

    Remove unused folder from repository

--- projects/unused/msg-stream_test.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.llms import Ollama
-llm = Ollama(model="tinyllama")
-query = "Tell me a joke"
-
-for chunks in llm.stream(query):
-    print(chunks, end='', flush=True)
-
-print()
-
